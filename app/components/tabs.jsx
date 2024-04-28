5ab38b483be2fd847ad541970718df5059eeebb0
Ilyas Farkhane <125192731+ilyasfarkhane-dev@users.noreply.github.com>
2024-04-28 19:23:21 +0100

    tabs edit 2

--- app/components/tabs.jsx
+++ app/components/tabs.jsx
@@ -24,13 +24,13 @@ const TabsComponent = ({ tabs }) => {
   return (
     <div className="bg-white flex justify-center items-center py-[80px]">
       <div className="max-w-[900px] flex flex-col gap-y-2 w-full">
-        <div className="bg-blue-900 p-1 text-sm rounded-full flex justify-between items-center gap-x-2 font-bold text-white shadow-lg">
+        <div className="bg-white p-1 text-sm rounded-full flex justify-between items-center gap-x-2 font-bold text-blue-900 shadow-lg">
           {tabs.map((item, index) => (
             <button
               key={index}
               onClick={() => setSelectedTab(index)}
-              className={`outline-none w-full p-2 rounded-full text-cneter focus:bg-white focus:text-blue-900 ${
-                selectedTab === index ? "ring-2 bg-white text-blue-900" : ""
+              className={`outline-none w-full p-2 rounded-full text-cneter focus:bg-blue-900 focus:text-white ${
+                selectedTab === index ? " bg-blue-900 text-white" : ""
               } `}
             >
               {item.title}
