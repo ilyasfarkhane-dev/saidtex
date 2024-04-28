272d66342c511164c0cf2083c9d87c6be117cb72
Ilyas Farkhane <125192731+ilyasfarkhane-dev@users.noreply.github.com>
2024-04-28 19:11:25 +0100

    tab edit

--- app/components/tabs.jsx
+++ app/components/tabs.jsx
@@ -22,7 +22,7 @@ const TabsComponent = ({ tabs }) => {
 
   return (
     <div className="bg-white flex justify-center items-center py-[80px]">
-      <div className="max-w-[900px] flex flex-col gap-y-2 w-full">
+      <div className="max-w-[400px] flex flex-col gap-y-2 w-full lg:max-w-[900px]">
         <div className="bg-blue-900 p-1 text-sm rounded-full flex justify-between items-center gap-x-2 font-bold text-white">
           {tabs.map((item, index) => (
             <button
