f4d26565a66da3a80e1999d346800c4c17b8ffb9
Ilyas Farkhane <125192731+ilyasfarkhane-dev@users.noreply.github.com>
2024-04-30 09:30:54 +0100

    filtter bar

--- app/components/partenaires.jsx
+++ app/components/partenaires.jsx
@@ -3,104 +3,111 @@
 import React, { useState } from "react";
 
 const Hero1 = () => {
-  // State to keep track of the active filter
-  const [activeFilter, setActiveFilter] = useState("*");
+  const [activeFilter, setActiveFilter] = useState("All");
 
-  // Function to handle filter click
   const handleFilterClick = (filter) => {
+    console.log("Selected category:", filter);
     setActiveFilter(filter);
   };
 
-  // Gallery items data
   const galleryItems = [
     { id: 1, category: "tissage" },
     { id: 2, category: "tissage" },
     { id: 3, category: "tissage" },
     { id: 6, category: "tissage" },
     { id: 7, category: "tissage" },
-    { id: 2, category: "web" },
-    { id: 3, category: "graphic" },
-    // Add more items as needed
+    { id: 11, category: "tissage" },
+    { id: 99, category: "tissage" },
+    { id: 66, category: "tissage" },
+    { id: 111, category: "tissage" },
+    { id: 222, category: "tissage" },
+    { id: "textape", category: "tissage" },
+    { id: 444, category: "tissage" },
+    { id: 5, category: "finissage" },
+    { id: 8, category: "finissage" },
+    { id: 9, category: "finissage" },
+    { id: 11, category: "finissage" },
+    { id: 12, category: "finissage" },
+    { id: 22, category: "finissage" },
+    { id: 33, category: "finissage" },
+    { id: 44, category: "finissage" },
+    { id: 55, category: "finissage" },
+    { id: 77, category: "finissage" },
+    { id: "textape", category: "finissage" },
+    { id: 4, category: "filature" },
+    { id: 11, category: "filature" },
+    { id: 88, category: "filature" },
+    { id: "heberlein", category: "filature" },
+    { id: 333, category: "filature" },
+    { id: "hemax", category: "filature" },
   ];
 
   return (
-    <>
-      <section className="works section-padding" data-scroll-index="2">
-        <div className="container">
-          <div className="row">
-            <div className="section-head offset-md-2 col-md-8 offset-lg-3 col-lg-6">
-              <h4>
-                <span>Nos</span> partenaires
-              </h4>
-            </div>
+    <section className="works section-padding" data-scroll-index="2">
+      <div className="">
+        <div className="row">
+          <div className="section-head offset-md-2 col-md-8 offset-lg-3 col-lg-6">
+            <h4>
+              <span>Nos</span> partenaires
+            </h4>
           </div>
         </div>
+      </div>
 
-        <div className="container-fluid">
-          <div className="row">
-            {/* filter links */}
-            <div className="filtering text-center mb-30 col-sm-12">
-              <div className="filter">
-                <span
-                  onClick={() => handleFilterClick("*")}
-                  className={activeFilter === "*" ? "active" : ""}
-                >
-                  All
-                </span>
-                <span
-                  onClick={() => handleFilterClick(".tissage")}
-                  className={activeFilter === ".tissage" ? "active" : ""}
-                >
-                  Tissage et bonneterie
-                </span>
-                <span
-                  onClick={() => handleFilterClick(".web")}
-                  className={activeFilter === ".web" ? "active" : ""}
-                >
-                  Finissage
-                </span>
-                <span
-                  onClick={() => handleFilterClick(".graphic")}
-                  className={activeFilter === ".graphic" ? "active" : ""}
-                >
-                  Filature
-                </span>
-              </div>
-            </div>
-
-            <div className="clearfix"></div>
-
-            {/* gallery */}
-            <div className="gallery full-width">
-              <div className="row">
-                {/* Render gallery items based on active filter */}
-                {galleryItems
-                  .filter(
-                    (item) =>
-                      activeFilter === "*" ||
-                      item.category === activeFilter.substring(1)
-                  )
-                  .map((item) => (
-                    <div
-                      key={item.id}
-                      className="col-lg-4 col-md-6 col-sm-12 mb-4"
-                    >
-                      <div className="item-img">
-                        <img
-                          src={`/images/portfolio/${item.id}.png`}
-                          alt="image"
-                          className="img-fluid"
-                        />
-                        {/* Add overlay and other elements */}
-                      </div>
-                    </div>
-                  ))}
-              </div>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="filtering text-center mb-30 col-sm-12">
+            <div className="filter">
+              <span
+                key="All"
+                onClick={() => handleFilterClick("All")}
+                className={activeFilter === "All" ? "active" : ""}
+              >
+                All
+              </span>
+              <span
+                onClick={() => handleFilterClick("tissage")}
+                className={activeFilter === "tissage" ? "active" : ""}
+              >
+                Tissage et bonneterie
+              </span>
+              <span
+                onClick={() => handleFilterClick("finissage")}
+                className={activeFilter === "finissage" ? "active" : ""}
+              >
+                Finissage
+              </span>
+              <span
+                onClick={() => handleFilterClick("filature")}
+                className={activeFilter === "filature" ? "active" : ""}
+              >
+                Filature
+              </span>
             </div>
           </div>
+
+          <div className="clearfix"></div>
+
+          <div className="gallery grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+            {galleryItems
+              .filter(
+                (item) =>
+                  activeFilter === "All" || item.category === activeFilter
+              )
+              .map(({ id }) => (
+                <div key={id} className="item-img">
+                  <img
+                    src={`/images/portfolio/${id}.png`}
+                    alt="image"
+                    className="img-fluid"
+                  />
+                  {/* Add overlay and other elements */}
+                </div>
+              ))}
+          </div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
