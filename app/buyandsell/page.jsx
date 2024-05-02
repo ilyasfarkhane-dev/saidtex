fd526fc2c6a84f2e8d524f50b5b466ee4d3f3efa
Ilyas Farkhane <125192731+ilyasfarkhane-dev@users.noreply.github.com>
2024-05-02 23:45:56 +0100

    button prix

--- app/buyandsell/page.jsx
+++ app/buyandsell/page.jsx
@@ -125,6 +125,40 @@ const ContactPage = () => {
           </div>
           <div>
             <label
+              htmlFor="type-de-machine"
+              className="block text-sm font-semibold leading-6 text-gray-900"
+            >
+              Année de la machine
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="text"
+                name="last-name"
+                id="last-name"
+                autoComplete="family-name"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="type-de-machine"
+              className="block text-sm font-semibold leading-6 text-gray-900"
+            >
+              Prix
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="text"
+                name="last-name"
+                id="last-name"
+                autoComplete="family-name"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label
               htmlFor="statue-de-machine"
               className="block text-sm font-semibold leading-6 text-gray-900"
             >
--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 
-const footer = () => {
+const Footer = () => {
   return (
-    <footer class="text-center pos-re ">
-      <div class="container mx-auto">
-        <a class="logo " href="#">
-          <img src="/images/saidtexlogo.png " alt="logo" />
+    <footer className="text-center pos-re">
+      <div className="container mx-auto">
+        <a className="logo" href="#">
+          <img src="/images/saidtexlogo.png" alt="logo" />
         </a>
 
-        <p>&copy; SAIDTEX © 2024 by FeizhouCom</p>
+        <p>
+          &copy; SAIDTEX © 2024 by{" "}
+          <a className="hover:text-blue-500" href="https://feizhoucom.com">
+            FeizhouCom
+          </a>
+        </p>
       </div>
 
-      <div class="curve curve-top curve-center "></div>
+      <div className="curve curve-top curve-center"></div>
     </footer>
   );
 };
 
-export default footer;
+export default Footer;
